a9deec0f9808f44d1994baa91f58891057892388
zaki <zaki47code@gmail.com>
2024-09-16 12:16:45 +0800

    error

---
+++ RecipeCube/RecipeCubeContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeCube;
+
+public partial class RecipeCubeContext : DbContext
+{
+    public RecipeCubeContext()
+    {
+    }
+
+    public RecipeCubeContext(DbContextOptions<RecipeCubeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=RecipeCube;TrustServerCertificate=True;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Orders__46596229A34EEA39");
+
+            entity.Property(e => e.OrderId)
+                .ValueGeneratedNever()
+                .HasColumnName("order_id");
+            entity.Property(e => e.OrderTime).HasColumnName("order_time");
+            entity.Property(e => e.Status).HasColumnName("status");
+            entity.Property(e => e.TotalAmount).HasColumnName("total_amount");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+        });
+        modelBuilder.HasSequence<int>("InventoryIDSeq");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
