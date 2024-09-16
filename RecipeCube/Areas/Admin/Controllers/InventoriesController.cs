be4cbf7fca3d2439b12a01313d830683c6cbdbe4
Andy Lin <andy0010502@gmail.com>
2024-09-16 17:02:34 +0800

    新增switch來指定個別的Controller，加入Inventories。

---
+++ RecipeCube/Areas/Admin/Controllers/InventoriesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RecipeCube.Models;
+
+namespace RecipeCube.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class InventoriesController : Controller
+    {
+        private readonly RecipeCubeContext _context;
+
+        public InventoriesController(RecipeCubeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Inventories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Inventories.ToListAsync());
+        }
+
+        public async Task<IActionResult> InventoryIndexPartial()
+        {
+            var Inventories = await _context.Inventories.ToListAsync();
+            return PartialView("_InventoryIndexPartial", Inventories);
+        }
+
+        // GET: Admin/Inventories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories
+                .FirstOrDefaultAsync(m => m.InventoryId == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // GET: Admin/Inventories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Inventories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InventoryId,GroupId,UserId,IngredientId,Quantity,ExpiryDate,IsExpiring,Visibility")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inventory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventory);
+        }
+
+        // GET: Admin/Inventories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Admin/Inventories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InventoryId,GroupId,UserId,IngredientId,Quantity,ExpiryDate,IsExpiring,Visibility")] Inventory inventory)
+        {
+            if (id != inventory.InventoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inventory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventoryExists(inventory.InventoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventory);
+        }
+
+        // GET: Admin/Inventories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories
+                .FirstOrDefaultAsync(m => m.InventoryId == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // POST: Admin/Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var inventory = await _context.Inventories.FindAsync(id);
+            if (inventory != null)
+            {
+                _context.Inventories.Remove(inventory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return _context.Inventories.Any(e => e.InventoryId == id);
+        }
+    }
+}
