6755b840780f7447649e28b9689f915729219b69
zaki <zaki47code@gmail.com>
2024-09-20 15:53:56 +0800

    訂單 訂單明細 功能皆完成 partial 僅查看訂單細項要再調整

--- RecipeCube/Areas/Admin/Controllers/OrderItemsController.cs
+++ RecipeCube/Areas/Admin/Controllers/OrderItemsController.cs
@@ -51,7 +51,7 @@ namespace RecipeCube.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-            return View(orderItem);
+            return PartialView("_EditPartial",orderItem);
         }
 
         // POST: Admin/OrderItems/Edit/5
@@ -59,11 +59,11 @@ namespace RecipeCube.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("OrderItemId,OrderId,ProductId,Quantity,Price")] OrderItem orderItem)
+        public JsonResult Edit(int id, [Bind("OrderItemId,OrderId,ProductId,Quantity,Price")] OrderItem orderItem)
         {
             if (id != orderItem.OrderItemId)
             {
-                return NotFound();
+                return new JsonResult(new { success = false, error = "ID不符合!" });
             }
 
             if (ModelState.IsValid)
@@ -71,36 +71,24 @@ namespace RecipeCube.Areas.Admin.Controllers
                 try
                 {
                     _context.Update(orderItem);
-                    await _context.SaveChangesAsync();
+                    _context.SaveChanges();
+                    return new JsonResult(new { success = true });
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!OrderItemExists(orderItem.OrderItemId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                    return new JsonResult(new { success = false, error = "發生錯誤!" });
+                }    
             }
-            return View(orderItem);
+            return new JsonResult(new
+            {
+                success = false,
+                error = "資料無效!",
+                errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)
+            });
         }
 
-        //GET: Admin/OrderItems
-        public async Task<IActionResult> Index(long orderid)
-        {
-            ViewBag.OrderId = orderid;
-            return View(await _context.OrderItems.ToListAsync());
-        }
-
-
-
-
-        // GET: Admin/OrderItems/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Admin/OrderItems/Delete/5
+        public async Task<IActionResult> DeletePartial(int? id)
         {
             if (id == null)
             {
@@ -114,8 +102,57 @@ namespace RecipeCube.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            return View(orderItem);
+            return PartialView("_DeletePartial",orderItem);
         }
+
+        // POST: Admin/OrderItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public JsonResult DeleteConfirmed(int id)
+        {
+            try { 
+            var orderItem = _context.OrderItems.Find(id);
+            if (orderItem != null)
+            {
+                _context.OrderItems.Remove(orderItem);
+            }
+
+            _context.SaveChanges();
+            return new JsonResult(new { success = true, error = "成功刪除!" });
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return new JsonResult(new { success = false, error = "發生錯誤!" });
+            }
+        }
+
+        ////GET: Admin/OrderItems
+        //public async Task<IActionResult> Index(long orderid)
+        //{
+        //    ViewBag.OrderId = orderid;
+        //    return View(await _context.OrderItems.ToListAsync());
+        //}
+
+
+
+
+        // GET: Admin/OrderItems/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var orderItem = await _context.OrderItems
+        //        .FirstOrDefaultAsync(m => m.OrderItemId == id);
+        //    if (orderItem == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(orderItem);
+        //}
 
         //// GET: Admin/OrderItems/Create
         //public IActionResult Create()
@@ -190,38 +227,38 @@ namespace RecipeCube.Areas.Admin.Controllers
         //    return View(orderItem);
         //}
 
-        // GET: Admin/OrderItems/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Admin/OrderItems/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var orderItem = await _context.OrderItems
-                .FirstOrDefaultAsync(m => m.OrderItemId == id);
-            if (orderItem == null)
-            {
-                return NotFound();
-            }
+        //    var orderItem = await _context.OrderItems
+        //        .FirstOrDefaultAsync(m => m.OrderItemId == id);
+        //    if (orderItem == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(orderItem);
-        }
+        //    return View(orderItem);
+        //}
 
-        // POST: Admin/OrderItems/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var orderItem = await _context.OrderItems.FindAsync(id);
-            if (orderItem != null)
-            {
-                _context.OrderItems.Remove(orderItem);
-            }
+        //// POST: Admin/OrderItems/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var orderItem = await _context.OrderItems.FindAsync(id);
+        //    if (orderItem != null)
+        //    {
+        //        _context.OrderItems.Remove(orderItem);
+        //    }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool OrderItemExists(int id)
         {
