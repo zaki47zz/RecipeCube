afd0e382875955908675147d0cbe58ebed36aff3
zaki <zaki47code@gmail.com>
2024-09-22 00:05:19 +0800

    新增ProductViewModel ProductIndexPartialView 調整為讀取ViewModel

--- RecipeCube/Areas/Admin/Controllers/ProductsController.cs
+++ RecipeCube/Areas/Admin/Controllers/ProductsController.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using AspNetCore;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using RecipeCube.Areas.Admin.ViewModels;
 using RecipeCube.Models;
 
 namespace RecipeCube.Areas.Admin.Controllers
@@ -24,7 +23,21 @@ namespace RecipeCube.Areas.Admin.Controllers
         public async Task<IActionResult> ProductIndexPartial()
         {
             var products = await _context.Products.ToListAsync();
-            return PartialView("_ProductIndexPartial", products);
+
+            // 將 Product 列表轉換為 ProductViewModel 列表
+            var viewModel = products.Select(product => new ProductViewModel
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                IngredientId = product.IngredientId,
+                Price = product.Price,
+                Stock = product.Stock,
+                Status = product.Status,
+                Photo = product.Photo,
+            }).ToList();
+
+            // 傳遞 ViewModle 列表到 PartialView
+            return PartialView("_ProductIndexPartial", viewModel);
         }
 
         // GET: Admin/Products/DetailsPartial
