6c065cf147624d30de3f5f4a7c95ad6ba695e31f
milkcrisp <a13822138222@gmail.com>
2024-09-23 02:22:40 +0800

    Revert "建立食材食譜關聯表的ViewModel"
    This reverts commit 2401fe0ca5e00f272cf490e2911b9a95666c1cc3.

--- RecipeCube/Areas/Admin/Controllers/RecipeIngredientsController.cs
+++ RecipeCube/Areas/Admin/Controllers/RecipeIngredientsController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using RecipeCube.Areas.Admin.ViewModels;
 using RecipeCube.Models;
 
 namespace RecipeCube.Areas.Admin.Controllers
@@ -25,152 +24,48 @@ namespace RecipeCube.Areas.Admin.Controllers
             return View(recipeIngredients);
         }
 
-        //public async Task<IActionResult> RecipeIngredientIndexPartial()
-        //{
-        //    var recipeIngredients = await _context.RecipeIngredients.ToListAsync();
-        //    return PartialView("_RecipeIngredientIndexPartial", recipeIngredients);
-        //}
         public async Task<IActionResult> RecipeIngredientIndexPartial()
         {
             var recipeIngredients = await _context.RecipeIngredients.ToListAsync();
-
-            var recipeIds = recipeIngredients.Select(ri => ri.RecipeId).Distinct().ToList();
-            var recipes = await _context.Recipes
-                .Where(r => recipeIds.Contains(r.RecipeId))
-                .ToListAsync();
-
-            var ingredientIds = recipeIngredients.Select(ri => ri.IngredientId).Distinct().ToList();
-            var ingredients = await _context.Ingredients
-                .Where(i => ingredientIds.Contains(i.IngredientId))
-                .ToListAsync();
-
-            var viewModel = recipeIngredients.Select(ri => new RecipeIngredientViewModel
-            {
-                RecipeIngredientId = ri.RecipeIngredientId,
-                RecipeId = ri.RecipeId,
-                IngredientId = ri.IngredientId,
-                Quantity = ri.Quantity,
-                RecipeName = recipes.FirstOrDefault(r => r.RecipeId == ri.RecipeId)?.RecipeName,
-                IngredientName = ingredients.FirstOrDefault(i => i.IngredientId == ri.IngredientId)?.IngredientName,
-                Unit = ingredients.FirstOrDefault(i => i.IngredientId == ri.IngredientId)?.Unit // 食材單位
-            }).ToList();
-
-            return PartialView("_RecipeIngredientIndexPartial", viewModel);
+            return PartialView("_RecipeIngredientIndexPartial", recipeIngredients);
         }
-
-
-        //// GET: Admin/RecipeIngredients/Details/5
-        //public async Task<IActionResult> DetailsPartial(int id)
-        //{
-        //    var recipeIngredient = await _context.RecipeIngredients
-        //        .FirstOrDefaultAsync(m => m.RecipeIngredientId == id);
-
-        //    if (recipeIngredient == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return PartialView("_DetailsPartial", recipeIngredient);
-        //}
 
         // GET: Admin/RecipeIngredients/Details/5
         public async Task<IActionResult> DetailsPartial(int id)
         {
-            try
+            var recipeIngredient = await _context.RecipeIngredients
+                .FirstOrDefaultAsync(m => m.RecipeIngredientId == id);
+
+            if (recipeIngredient == null)
             {
-                // 查詢 RecipeIngredient
-                var recipeIngredient = await _context.RecipeIngredients.FirstOrDefaultAsync(m => m.RecipeIngredientId == id);
-                if (recipeIngredient == null)
-                {
-                    return NotFound();
-                }
-
-                // 查詢對應的 Recipe 名稱
-                var recipe = await _context.Recipes.FirstOrDefaultAsync(r => r.RecipeId == recipeIngredient.RecipeId);
-                if (recipe == null)
-                {
-                    throw new Exception("Recipe not found.");
-                }
-
-                // 查詢對應的 Ingredient 名稱
-                var ingredient = await _context.Ingredients.FirstOrDefaultAsync(i => i.IngredientId == recipeIngredient.IngredientId);
-                if (ingredient == null)
-                {
-                    throw new Exception("Ingredient not found.");
-                }
-
-                // 創建 ViewModel 並填充資料
-                var viewModel = new RecipeIngredientViewModel
-                {
-                    RecipeIngredientId = recipeIngredient.RecipeIngredientId,
-                    RecipeId = recipeIngredient.RecipeId,
-                    IngredientId = recipeIngredient.IngredientId,
-                    Quantity = recipeIngredient.Quantity,
-                    RecipeName = recipe.RecipeName,
-                    IngredientName = ingredient.IngredientName,
-                    Unit = ingredient.Unit,
-                };
-
-                return PartialView("_DetailsPartial", viewModel);
+                return NotFound();
             }
-            catch (Exception ex)
-            {
-                // 記錄異常
-                Console.WriteLine(ex.Message);
-                return StatusCode(500, "Internal server error");
-            }
+
+            return PartialView("_DetailsPartial", recipeIngredient);
         }
 
-
-
         // GET: Admin/RecipeIngredients/CreatePartial
-        public async Task<IActionResult> CreatePartial()
+        public IActionResult CreatePartial()
         {
-            // 加載可用的食譜和食材
-            ViewBag.Recipes = await _context.Recipes.Select(r => new { r.RecipeId, r.RecipeName }).ToListAsync();
-            ViewBag.Ingredients = await _context.Ingredients.Select(i => new { i.IngredientId, i.IngredientName, i.Unit }).ToListAsync();
-
-            return PartialView("_CreatePartial", new RecipeIngredientViewModel());
+            return PartialView("_CreatePartial");
         }
 
         // POST: Admin/RecipeIngredients/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("RecipeId,IngredientId,Quantity")] RecipeIngredientViewModel viewModel)
+        public async Task<IActionResult> Create([Bind("RecipeIngredientId,RecipeId,IngredientId,Quantity")] RecipeIngredient recipeIngredient)
         {
             if (ModelState.IsValid)
             {
-                var recipeIngredient = new RecipeIngredient
-                {
-                    RecipeId = viewModel.RecipeId,
-                    IngredientId = viewModel.IngredientId,
-                    Quantity = viewModel.Quantity
-                };
-
                 _context.Add(recipeIngredient);
                 await _context.SaveChangesAsync();
                 return Json(new { success = true });
             }
 
-            // 如果驗證失敗，重新加載可用的食譜和食材，並返回表單
-            ViewBag.Recipes = await _context.Recipes.Select(r => new { r.RecipeId, r.RecipeName }).ToListAsync();
-            ViewBag.Ingredients = await _context.Ingredients.Select(i => new { i.IngredientId, i.IngredientName, i.Unit }).ToListAsync();
-
-            return PartialView("_CreatePartial", viewModel);
+            return PartialView("_CreatePartial", recipeIngredient);
         }
 
         // GET: Admin/RecipeIngredients/EditPartial/5
-        //public async Task<IActionResult> EditPartial(int id)
-        //{
-        //    var recipeIngredient = await _context.RecipeIngredients.FindAsync(id);
-
-        //    if (recipeIngredient == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return PartialView("_EditPartial", recipeIngredient);
-        //}
         public async Task<IActionResult> EditPartial(int id)
         {
             var recipeIngredient = await _context.RecipeIngredients.FindAsync(id);
@@ -180,36 +75,8 @@ namespace RecipeCube.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            // 查詢對應的 Recipe 和 Ingredient 名稱
-            var recipe = await _context.Recipes
-                .Where(r => r.RecipeId == recipeIngredient.RecipeId)
-                .Select(r => new { r.RecipeId, r.RecipeName })
-                .FirstOrDefaultAsync();
-
-            var ingredient = await _context.Ingredients
-                .Where(i => i.IngredientId == recipeIngredient.IngredientId)
-                .Select(i => new { i.IngredientId, i.IngredientName, i.Unit })
-                .FirstOrDefaultAsync();
-
-            // 創建 ViewModel 並將數據傳入
-            var viewModel = new RecipeIngredientViewModel
-            {
-                RecipeIngredientId = recipeIngredient.RecipeIngredientId,
-                RecipeId = recipeIngredient.RecipeId,
-                IngredientId = recipeIngredient.IngredientId,
-                Quantity = recipeIngredient.Quantity,
-                RecipeName = recipe?.RecipeName, // 使用三元運算符來避免 null 問題
-                IngredientName = ingredient?.IngredientName,
-                Unit = ingredient?.Unit, // 設置單位
-            };
-
-            // 加入從數據庫獲取的可用食譜和食材名稱
-            ViewBag.Recipes = await _context.Recipes.Select(r => new { r.RecipeId, r.RecipeName }).ToListAsync();
-            ViewBag.Ingredients = await _context.Ingredients.Select(i => new { i.IngredientId, i.IngredientName, i.Unit }).ToListAsync();
-
-            return PartialView("_EditPartial", viewModel);
+            return PartialView("_EditPartial", recipeIngredient);
         }
-
 
         // POST: Admin/RecipeIngredients/Edit
         [HttpPost]
@@ -253,6 +120,7 @@ namespace RecipeCube.Areas.Admin.Controllers
         }
 
 
+        // GET: Admin/RecipeIngredients/DeletePartial/5
         public async Task<IActionResult> DeletePartial(int? id)
         {
             if (id == null)
@@ -262,38 +130,13 @@ namespace RecipeCube.Areas.Admin.Controllers
 
             var recipeIngredient = await _context.RecipeIngredients
                 .FirstOrDefaultAsync(m => m.RecipeIngredientId == id);
-
             if (recipeIngredient == null)
             {
                 return NotFound();
             }
 
-            // 查詢 Recipe 和 Ingredient 的名稱
-            var recipe = await _context.Recipes
-                .Where(r => r.RecipeId == recipeIngredient.RecipeId)
-                .Select(r => new { r.RecipeId, r.RecipeName })
-                .FirstOrDefaultAsync();
-
-            var ingredient = await _context.Ingredients
-                .Where(i => i.IngredientId == recipeIngredient.IngredientId)
-                .Select(i => new { i.IngredientId, i.IngredientName, i.Unit })
-                .FirstOrDefaultAsync();
-
-            // 創建 ViewModel 並將資料填充
-            var viewModel = new RecipeIngredientViewModel
-            {
-                RecipeIngredientId = recipeIngredient.RecipeIngredientId,
-                RecipeId = recipe?.RecipeId,
-                IngredientId = ingredient?.IngredientId,
-                Quantity = recipeIngredient.Quantity,
-                RecipeName = recipe?.RecipeName,
-                IngredientName = ingredient?.IngredientName,
-                Unit = ingredient?.Unit
-            };
-
-            return PartialView("_DeletePartial", viewModel);
+            return PartialView("_DeletePartial", recipeIngredient);
         }
-
 
         // POST: Admin/RecipeIngredients/Delete/5
         [HttpPost, ActionName("Delete")]
