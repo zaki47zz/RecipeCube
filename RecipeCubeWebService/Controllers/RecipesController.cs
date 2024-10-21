e1ca6ec0b1d46b18d57ab03d7805be4efa0f779f
milkcrisp <a13822138222@gmail.com>
2024-10-21 11:19:07 +0800

    Recipe POST&PUT

--- RecipeCubeWebService/Controllers/RecipesController.cs
+++ RecipeCubeWebService/Controllers/RecipesController.cs
@@ -126,15 +126,72 @@ namespace RecipeCubeWebService.Controllers
         }
         // PUT: api/Recipes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // PUT: api/Recipes/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+        public async Task<IActionResult> PutRecipe(int id, RecipeDto recipeDto)
         {
-            if (id != recipe.RecipeId)
+            if (id != recipeDto.RecipeId)
             {
                 return BadRequest();
             }
 
+            var recipe = await _context.Recipes.FirstOrDefaultAsync(r => r.RecipeId == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            // Update recipe properties
+            recipe.RecipeName = recipeDto.RecipeName;
+            recipe.UserId = recipeDto.UserId;
+            recipe.IsCustom = recipeDto.IsCustom;
+            recipe.Restriction = recipeDto.Restriction;
+            recipe.WestEast = recipeDto.WestEast;
+            recipe.Category = recipeDto.Category;
+            recipe.DetailedCategory = recipeDto.DetailedCategory;
+            recipe.Steps = recipeDto.Steps;
+            recipe.Seasoning = recipeDto.Seasoning;
+            recipe.Visibility = recipeDto.Visibility;
+            recipe.Photo = recipeDto.Photo;
+            recipe.Status = recipeDto.Status;
+
             _context.Entry(recipe).State = EntityState.Modified;
+
+            // Update RecipeIngredients
+            var existingIngredients = await _context.RecipeIngredients
+                .Where(ri => ri.RecipeId == recipe.RecipeId)
+                .ToListAsync();
+
+            // Remove ingredients that are no longer associated
+            foreach (var existingIngredient in existingIngredients)
+            {
+                if (!recipeDto.SelectedIngredients.Contains(existingIngredient.IngredientId ?? 0))
+                {
+                    _context.RecipeIngredients.Remove(existingIngredient);
+                }
+            }
+
+            // Add or update ingredients
+            foreach (var ingredientId in recipeDto.SelectedIngredients)
+            {
+                var existingIngredient = existingIngredients.FirstOrDefault(ri => ri.IngredientId == ingredientId);
+                if (existingIngredient == null)
+                {
+                    // Add new ingredient
+                    var recipeIngredient = new RecipeIngredient
+                    {
+                        RecipeId = recipe.RecipeId,
+                        IngredientId = ingredientId,
+                        Quantity = recipeDto.IngredientQuantities.ContainsKey(ingredientId) ? recipeDto.IngredientQuantities[ingredientId] : 0M
+                    };
+                    _context.RecipeIngredients.Add(recipeIngredient);
+                }
+                else
+                {
+                    // Update existing ingredient quantity
+                    existingIngredient.Quantity = recipeDto.IngredientQuantities.ContainsKey(ingredientId) ? recipeDto.IngredientQuantities[ingredientId] : 0M;
+                }
+            }
 
             try
             {
@@ -152,17 +209,63 @@ namespace RecipeCubeWebService.Controllers
                 }
             }
 
-            return NoContent();
+            return Ok("修改食譜資訊成功");
         }
 
         // POST: api/Recipes
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // POST: api/Recipes
         [HttpPost]
-        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        public async Task<ActionResult<Recipe>> PostRecipe(RecipeDto recipeDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Create new Recipe entity
+            var recipe = new Recipe
+            {
+                RecipeName = recipeDto.RecipeName,
+                UserId = recipeDto.UserId,
+                IsCustom = recipeDto.IsCustom,
+                Restriction = recipeDto.Restriction,
+                WestEast = recipeDto.WestEast,
+                Category = recipeDto.Category,
+                DetailedCategory = recipeDto.DetailedCategory,
+                Steps = recipeDto.Steps,
+                Seasoning = recipeDto.Seasoning,
+                Visibility = recipeDto.Visibility,
+                Photo = recipeDto.Photo,
+                Status = recipeDto.Status
+            };
+
             _context.Recipes.Add(recipe);
             await _context.SaveChangesAsync();
 
+            // Handle selected ingredients and their quantities
+            if (recipeDto.SelectedIngredients != null && recipeDto.IngredientQuantities != null)
+            {
+                var ingredientIds = recipeDto.SelectedIngredients.Distinct().ToList();
+
+                foreach (var ingredientId in ingredientIds)
+                {
+                    if (recipeDto.IngredientQuantities.ContainsKey(ingredientId))
+                    {
+                        var quantity = recipeDto.IngredientQuantities[ingredientId];
+
+                        var recipeIngredient = new RecipeIngredient
+                        {
+                            RecipeId = recipe.RecipeId,
+                            IngredientId = ingredientId,
+                            Quantity = quantity
+                        };
+                        _context.RecipeIngredients.Add(recipeIngredient);
+                    }
+                }
+            }
+
+            await _context.SaveChangesAsync();
             return CreatedAtAction("GetRecipe", new { id = recipe.RecipeId }, recipe);
         }
 
