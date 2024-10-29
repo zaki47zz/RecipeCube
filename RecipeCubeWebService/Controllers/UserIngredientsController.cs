01300801ac19ca70b8599865910f20646f8de7cf
Willy <Willy@ispan.com.tw>
2024-10-29 16:22:33 +0800

    1) 填入偏好&不可食用食材api完成
    後續實作功能:
       - 第三方註冊(選完葷素才建立)
    ＊ 聯絡管理員(前端傳接email api)
       - 帳號鎖定申訴
       - 停權恢復
       - Bug回報
    ＊ 修改使用者基本資料功能
       - 加入群組&創建群組
       - 填入偏好&不可食用食材
    
    待優化功能:
    
    登入:不刷新頁面，只動態更新Navber顯示登入內容
    登入&註冊使用 VeeValidate來實現多語系資料驗證
    驗證email版面美化
    前端重設密碼token過期無法捕捉後端api message
    勾選是否紀錄偏好&不可食用食材

--- RecipeCubeWebService/Controllers/UserIngredientsController.cs
+++ RecipeCubeWebService/Controllers/UserIngredientsController.cs
@@ -113,16 +113,66 @@ namespace RecipeCubeWebService.Controllers
         //    return NoContent();
         //}
 
-        //// POST: api/ExclusiveIngredients
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<ExclusiveIngredient>> PostExclusiveIngredient(ExclusiveIngredient exclusiveIngredient)
-        //{
-        //    _context.ExclusiveIngredients.Add(exclusiveIngredient);
-        //    await _context.SaveChangesAsync();
+        // POST: /api/UserIngredients/ExclusiveIngredientsAdd
+        [HttpPost("ExclusiveIngredientsAdd")]
+        public async Task<IActionResult> ExclusiveIngredientsAddDTO(IngredientsAddDTO ExclusiveIngredientsAdd)
+        {
+            if(ExclusiveIngredientsAdd ==null)
+            {
+                return BadRequest("Invalid signup data.");
+            }
+            var existingUser = await _context.Users.SingleOrDefaultAsync(u => u.Id == ExclusiveIngredientsAdd.User_Id);
+            if (existingUser == null)
+            {
+                return NotFound(new { Message = "User not found" });
+            }
 
-        //    return CreatedAtAction("GetExclusiveIngredient", new { id = exclusiveIngredient.ExclusiveIngredientId }, exclusiveIngredient);
-        //}
+            var userId = ExclusiveIngredientsAdd.User_Id;
+            var IngredientsId = ExclusiveIngredientsAdd.Ingredient_Id;
+
+            var newExclusiveIngredient = new ExclusiveIngredient
+            {
+                UserId = ExclusiveIngredientsAdd.User_Id,
+                IngredientId = ExclusiveIngredientsAdd.Ingredient_Id
+            };
+
+            // 將新項目新增至資料庫
+            _context.ExclusiveIngredients.Add(newExclusiveIngredient);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Exclusive ingredient added successfully" });
+        }
+
+
+        // POST: /api/UserIngredients/PreferedIngredientsAdd
+        [HttpPost("PreferedIngredientsAdd")]
+        public async Task<IActionResult> PreferedIngredientsAddDTO(IngredientsAddDTO PreferedIngredientsAdd)
+        {
+            if (PreferedIngredientsAdd == null)
+            {
+                return BadRequest("Invalid signup data.");
+            }
+            var existingUser = await _context.Users.SingleOrDefaultAsync(u => u.Id == PreferedIngredientsAdd.User_Id);
+            if (existingUser == null)
+            {
+                return NotFound(new { Message = "User not found" });
+            }
+
+            var userId = PreferedIngredientsAdd.User_Id;
+            var IngredientsId = PreferedIngredientsAdd.Ingredient_Id;
+
+            var newPreferedIngredient = new PreferedIngredient
+            {
+                UserId = PreferedIngredientsAdd.User_Id,
+                IngredientId = PreferedIngredientsAdd.Ingredient_Id
+            };
+
+            // 將新項目新增至資料庫
+            _context.PreferedIngredients.Add(newPreferedIngredient);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Exclusive ingredient added successfully" });
+        }
 
         // DELETE: /api/UserIngredients/ExclusiveIngredientsDelete
         [HttpDelete("ExclusiveIngredientsDelete")]
---
+++ RecipeCubeWebService/DTO/IngredientsAddDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeCubeWebService.DTO
+{
+    public class IngredientsAddDTO
+    {
+        public required string User_Id { get; set; }
+        public required int Ingredient_Id { get; set; }
+
+    }
+}
