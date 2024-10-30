744e12acfdf372bb5736006e0380ddc31891ff6e
Willy <Willy@ispan.com.tw>
2024-10-30 15:21:31 +0800

    1) 創建群組
    後續實作功能:
       - 第三方註冊(選完葷素才建立)
    ＊ 聯絡管理員(前端傳接email api)
       - 帳號鎖定申訴
       - 停權恢復
       - Bug回報
    ＊ 修改使用者基本資料功能
       - 加入群組&創建群組
       - 勾選是否紀錄偏好&不可食用食材
       - 填入偏好&不可食用食材
    
    待優化功能:
    
    登入:不刷新頁面，只動態更新Navber顯示登入內容
    登入&註冊使用 VeeValidate來實現多語系資料驗證
    驗證email版面美化
    前端重設密碼token過期無法捕捉後端api message
    將新增偏好&不可食用食材改成ajax

---
+++ RecipeCubeWebService/DTO/CreateGroupDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeCubeWebService.DTO
+{
+    public class CreateGroupDTO
+    {
+        public string group_name { get; set; }
+        public string group_Admin_Id { get; set; }
+    }
+}
